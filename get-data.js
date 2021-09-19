c6644f719f11aa21aa9e97337141565e9dd42d2b
Charles Maye <charlesmayejr@gmail.com>
2021-09-19 18:59:03 -0500

    updated get data function

--- get-data.js
+++ get-data.js
@@ -1,8 +1,13 @@
 const axios = require('axios');
 const fs = require('fs');
 
+const local = 'http://localhost:1337/portfolio';
+const heroku = 'http://10.0.0.114:1337/portfolio';
+
 async function loadData() {
-    const response = await axios.get("http://localhost:1337/portfolio")
+    const response = await axios.get(
+        process.env.NODE_ENV === 'production' ? heroku : local
+    )
 
     // portfolio JSON
     const portfolio = JSON.stringify(response.data);
